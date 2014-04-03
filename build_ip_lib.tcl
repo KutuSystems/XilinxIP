438297f81805fc954067dfc2d85bcd77cb8f73f4
gregsmart <Greg.Smart@kutu.com.au>
2014-04-03 17:42:06 +1100

    added axi_hdmi_tx_16b to script

--- build_ip_lib.tcl
+++ build_ip_lib.tcl
@@ -10,6 +10,9 @@ close_project
 cd ../util_hdmi_tx_24b_v1_00_a
 source build_ip.tcl
 close_project
+cd ../axi_hdmi_tx_16b_v2_00_a
+source build_ip.tcl
+close_project
 cd ../axi_lite_controller_v1_00_a
 source build_ip.tcl
 close_project
